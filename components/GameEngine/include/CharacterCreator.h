249e6ebdde4226ef7ef46cc165cf4bdfb4963332
KapJ1coH <tima.mtl@gmail.com>
2024-04-04 17:08:29 -0400

    added blank charactercreator

---
+++ components/GameEngine/include/CharacterCreator.h
@@ -0,0 +1,11 @@
+/**
+ * @file CharacterCreator.h
+ * @author Tymofiy Kovalenko
+ * @date 04-Apr-2024
+ * @brief
+ */
+
+#ifndef PROJECT_CHARACTERCREATOR_H
+#define PROJECT_CHARACTERCREATOR_H
+
+#endif//PROJECT_CHARACTERCREATOR_H
